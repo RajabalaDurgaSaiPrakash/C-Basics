83d5cf7deff23849a271a78cef777858f08afb04
RajabalaDurgaSaiPrakash <92287848+RajabalaDurgaSaiPrakash@users.noreply.github.com>
2021-10-20 10:53:09 +0530

    Add files via upload

---
+++ Untitled1.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+void main()
+{
+	//variable declaration
+	int age;
+	float cgpa;
+	char initial;
+	
+	//initialization
+	age = 20;
+	cgpa = 8.8;
+	initial = 'R';
+	
+	//output
+	printf("%d\n", age);
+	printf("%f\n", cgpa);
+	printf("%c\n", initial);
+}
+
