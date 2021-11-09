22cfe269b221ad307c73dae126288e8de6ed6c8f
RajabalaDurgaSaiPrakash <92287848+RajabalaDurgaSaiPrakash@users.noreply.github.com>
2021-11-09 10:42:29 +0530

    Add files via upload

---
+++ ascii.c
@@ -0,0 +1,8 @@
+//to know the ascii number for given character
+#include<stdio.h>
+void main()
+{
+char n;
+	n='Z';
+	printf("corresponding ASCII number for %c is %d",n,n);
+} 
---
+++ scanf enter 3numbers.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+	int a,b,c;
+	printf("enter 1st number:");
+	scanf("%d",&a);
+	printf("enter 2nd number:");
+	scanf("%d",&b);
+	printf("enter 3rd number:");
+	scanf("%d",&c);
+	
+	printf("number1:%d",a);
+	printf("\nnumber2:%d",b);
+	printf("\nnumber3:%d",c);
+}
