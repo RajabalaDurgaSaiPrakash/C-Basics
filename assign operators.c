f34aac6c9def4fde7b7fb583e579483bdc60bfca
RajabalaDurgaSaiPrakash <92287848+RajabalaDurgaSaiPrakash@users.noreply.github.com>
2021-11-17 10:42:36 +0530

    Add files via upload

---
+++ assign operators.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+void main()
+{
+	int x = 10, y= 4;
+	x += y;
+	y += x;
+	printf("x = %d\ny = %d",x,y);
+}
---
+++ assign operators3.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+	int x,y,z;
+	x=2;
+	y=3;
+	z=4;
+	x += y; //6,3,4
+	x -= y; //-1,3,4
+	printf("x = %d\ny = %d\nz = %d",x,y,z);
+}
