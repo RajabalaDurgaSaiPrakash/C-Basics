a8c505d419921f8f9fbfb075af5a67c3b06fbfa5
RajabalaDurgaSaiPrakash <92287848+RajabalaDurgaSaiPrakash@users.noreply.github.com>
2021-12-14 09:35:27 +0530

    Add files via upload

---
+++ pseudocode 2.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+void main()
+{
+	int a,b,c;
+	printf("enter the numbers:");
+	scanf("%d%d%d",&a,&b,&c);
+	if(a>b || (c+a) < (b-c))
+	{
+		b=(12+10)+a;
+		b=c+b;
+	}
+	else
+	{
+		c=c+c;
+	}
+	{
+		
+	printf("%d",a+b+c);
+}
+}
