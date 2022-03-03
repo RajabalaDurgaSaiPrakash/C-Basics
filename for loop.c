c4ace598dd6b5287ddc7301662c46b290464e4ff
RajabalaDurgaSaiPrakash <92287848+RajabalaDurgaSaiPrakash@users.noreply.github.com>
2022-03-03 10:49:03 +0530

    Add files via upload

---
+++ for loop.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void main()
+{
+	int a;
+	for(a=1;a<=25;++a)
+	{
+		printf("%d\n",a);
+	}
+	return 0;
+}
---
+++ while loop.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+	int a=1;
+	while(a<=15)
+	{
+		printf("%d\n",a);
+		a++;
+	}
+	return 0;
+}
